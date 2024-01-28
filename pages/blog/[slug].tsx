7ea76aa94e83ca16e57cc8305c319001924cfac6
chanaka <chanakadrathnayaka@gmail.com>
2024-01-28 10:17:05 -0800

    Initial Commit

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,14 +1,14 @@
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import ErrorPage from "next/error";
 import Container from "../../components/container";
 import PostBody from "../../components/post-body";
 import Header from "../../components/header";
 import PostHeader from "../../components/post-header";
 import Layout from "../../components/layout";
-import { getPostBySlug, getAllPosts } from "../../lib/api";
+import {getAllPosts, getPostBySlug} from "../../lib/api";
 import PostTitle from "../../components/post-title";
 import Head from "next/head";
-import { CMS_NAME } from "../../lib/constants";
+import {SITE_NAME} from "../../lib/constants";
 import markdownToHtml from "../../lib/markdownToHtml";
 import type PostType from "../../interfaces/post";
 
@@ -20,7 +20,7 @@ type Props = {
 
 export default function Post({ post, morePosts, preview }: Props) {
   const router = useRouter();
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+  const title = `${post.title} | ${SITE_NAME}`;
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
   }
