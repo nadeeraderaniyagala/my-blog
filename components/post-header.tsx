38a16d59ae69a3389f4783024f1010d8a47bffc2
chanaka <chanakadrathnayaka@gmail.com>
2024-01-28 12:50:46 -0800

    Updated and Created dates are introduced

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -8,28 +8,40 @@ type Props = {
   title: string;
   coverImage: string;
   createdOn: string;
-  updatedOn: string;
+  updatedOn?: string;
   author: Author;
 };
 
-const PostHeader = ({title, coverImage, createdOn, updatedOn, author}: Props) => {
+function renderDate(updatedOn: string, createdOn: string) {
   return (
-    <>
-      <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
-        <DateFormatter dateString={createdOn}/>
-        <DateFormatter dateString={updatedOn}/>
+      <div>
+        {updatedOn ? 'Updated' : 'Created'} on&nbsp;
+        <DateFormatter dateString={updatedOn || createdOn}/>
       </div>
-      <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} />
-      </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
+  );
+}
+
+const PostHeader = ({title, coverImage, createdOn, updatedOn, author}: Props) => {
+
+  const date = renderDate(updatedOn, createdOn)
+  return (
+      <>
+        <PostTitle>{title}</PostTitle>
+        <div className="hidden md:flex md:mb-12 justify-between">
+          <div><Avatar name={author.name} picture={author.picture}/></div>
+          <div className="text-gray-400 text-sm">
+            {date}
+          </div>
         </div>
-      </div>
-    </>
+        <div className="mb-8 md:mb-16 sm:mx-0">
+          <CoverImage title={title} src={coverImage}/>
+        </div>
+        <div className="max-w-2xl mx-auto">
+          <div className="block md:hidden mb-6">
+            <Avatar name={author.name} picture={author.picture}/>
+          </div>
+        </div>
+      </>
   );
 };
 
